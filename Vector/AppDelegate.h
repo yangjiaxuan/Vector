01673f4d05d5a453d8dd6c2a9895ff3230abc4ae
yangen <yang_sen_ysys@163.com>
2017-12-29 21:34:43 +0800

    Initial Commit

---
+++ Vector/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Vector
+//
+//  Created by 杨森 on 2017/12/29.
+//  Copyright © 2017年 杨森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Vector/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vector
+//
+//  Created by 杨森 on 2017/12/29.
+//  Copyright © 2017年 杨森. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
